459445a3acdccbdaf35f2faf5487c56af5242242
PuQing <me@puqing.work>
2024-01-14 22:19:12 +0800

    Add user API endpoints

--- bemore/web/api/router.py
+++ bemore/web/api/router.py
@@ -1,7 +1,8 @@
 from fastapi.routing import APIRouter
 
-from bemore.web.api import echo, monitoring
+from bemore.web.api import echo, monitoring, users
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+api_router.include_router(users.router, prefix="/user", tags=["user"])
---
+++ bemore/web/api/users/__init__.py
@@ -0,0 +1,4 @@
+"""Echo API."""
+from bemore.web.api.users.views import router
+
+__all__ = ["router"]
