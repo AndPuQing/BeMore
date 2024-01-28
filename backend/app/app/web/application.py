99fdf7d1c3534deccef4a1b3e7ca6180bbfb760e
PuQing <me@puqing.work>
2024-01-29 03:07:58 +0800

    Refactor application lifespan management

--- backend/app/app/web/application.py
+++ backend/app/app/web/application.py
@@ -1,11 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.responses import UJSONResponse
 
 import app as appx  # just to get version
 from app.core.config import settings
+from app.db.init_db import init
 from app.log import configure_logging
 from app.web.api.router import api_router
-from app.web.lifetime import register_shutdown_event, register_startup_event
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Context manager for the lifespan of the application.
+
+    :param app: current FastAPI application.
+    """
+    app.middleware_stack = None
+    app.middleware_stack = app.build_middleware_stack()
+    init()
+    yield
+    pass
 
 
 def get_app() -> FastAPI:
@@ -24,11 +40,8 @@ def get_app() -> FastAPI:
         redoc_url=f"{settings.API_STR}/redoc",
         openapi_url=f"{settings.API_STR}/openapi.json",
         default_response_class=UJSONResponse,
+        lifespan=lifespan,
     )
-
-    # Adds startup and shutdown events.
-    register_startup_event(app)
-    register_shutdown_event(app)
 
     # Main router for the API.
     app.include_router(router=api_router, prefix=f"{settings.API_STR}")
